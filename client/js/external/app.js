d6da7f001145a70fa02d82b8f58e16df0e61440a
Jesse Ditson <jesse.ditson@gmail.com>
2012-04-11 12:08:31 -0700

    adding support for default routes, fixing named routes, experimental support for regex client side routes.

--- client/js/external/app.js
+++ client/js/external/app.js
@@ -3,38 +3,59 @@ define(
   function(Router,viewInfo){
     var viewsArray = Array.prototype.slice.call(viewInfo.views),
         viewObject = viewInfo.routes,
-        app = new Router(),
+        router = new Router(),
         viewRoutes = {},
         views = {},
         viewNum = 0;
     // stick views in an object
     for(var viewPath in viewObject){
       var viewName,
+          route,
+          raw = ['main'],
           viewNameMatches = viewPath.match(/\/([^\/]+)(\.js)?$/);
       if(!viewNameMatches || !viewNameMatches[1]){
         throw new Error('view name for view '+viewPath+' is malformed. Failed to load views.');
         return;
       }
       viewName = viewNameMatches[1];
+      route = viewObject[viewPath];
       views[viewName] = viewsArray[viewNum];
-      viewRoutes[viewName] = viewObject[viewPath];
+      // detect if this route a regex (just checks for slashes right now)
+      if(route.match(/^\/(.+?[^\\]\/)+\w*$/)){
+        // convert to regexp object
+        var pattern = route.replace(/^\/(.+?)\/\w*$/,"$1").replace(/(\\)/,"\$1"),
+            flags = route.replace(/^\/.+?\/(\w*)$/,"$1");
+        route = new RegExp(viewName + '/' +pattern,flags);
+      } else if(!~raw.indexOf(viewName)) {
+        route = viewName + ((route) ? '/' + route : "");
+      }
+      viewRoutes[viewName] = route;
       viewNum ++;
     }
+    // build an object for the client to interact with
+    var app = {
+      views : views,
+      router : router,
+      start : router.start,
+      defaultRoute : function(){
+        this.show();
+      }
+    };
     // loop through views and set up the basics.
     for(var name in views){
       var view = views[name];
-      view.router = app;
-      app.route(viewRoutes[name],name,_.bind(function(){
-        if(this.route){
-          this.route.apply(this,arguments);
-        } else {
-          this.show();
-        }
-      },view));
+      if(!view){
+        console.error('Failed Loading view %s',name);
+      } else {
+        view.router = router;
+        router.route(viewRoutes[name],name,_.bind(function(){
+          if(this.route){
+            this.route.apply(this,arguments);
+          } else {
+            app.defaultRoute.apply(this,arguments);
+          }
+        },view));
+      }
     }
-    return {
-      views : views,
-      router : app,
-      start : app.start
-    };
+    return app;
   });
